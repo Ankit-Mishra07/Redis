17d632aa97233e3d9348fe011ec8d90a384540dd
Ankit-Mishra07 <ankitmi468@gmail.com>
2021-12-10 23:59:19 +0530

    patch and getOne item

--- assignment/src/controllers/product.controller.js
+++ assignment/src/controllers/product.controller.js
@@ -32,6 +32,37 @@ router.post("", async (req, res) => {
     return res.status(200).send(product)
 })
 
+router.get("/:id", (req, res) => {
+    redis.get(`products.${req.params.id}`, async (err, product) => {
+
+        if(err) {console.log(err)}
+
+        if(product) {
+            return res.status(200).send({cachedData: JSON.parse(product)})
+        }
+
+        const products = await Product.findById(req.params.id).lean().exec()
+
+        redis.set(`products.${req.params.id}`, JSON.stringify(products))
+
+        return res.status(201).send({dbProduct : products})
+    })
+})
+
+
+router.patch("/:id", async (req, res) => {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new :true});
+
+    redis.set(`products.${req.params.id}`, JSON.stringify(product))
+
+    const products = await Product.find().lean().exec()
+
+    redis.set("products", JSON.stringify(products))
+
+    res.status(201).send(product)
+})
+
+
 
 
 
