dd15551b7c3ef71b572a1a22644b96c68f9bd19a
Ankit-Mishra07 <ankitmi468@gmail.com>
2021-12-10 23:17:17 +0530

    post controller

--- assignment/src/controllers/product.controller.js
+++ assignment/src/controllers/product.controller.js
@@ -8,7 +8,6 @@ const Product = require("../models/product.model")
 router.get("",  (req, res) => {
 
     redis.get("products", async function(err, prods) {
-        console.log("product")
         
         if(err) { console.log(err) }
 
@@ -21,6 +20,19 @@ router.get("",  (req, res) => {
         return res.status(200).send(products)
     })
 })
+
+
+router.post("", async (req, res) => {
+    
+    const product = await Product.create(req.body)
+    const products = await Product.find().lean().exec()
+
+    redis.set("products", JSON.stringify(products))
+
+    return res.status(200).send(product)
+})
+
+
 
 
 
